de060422bf33df1f072d689ef56edff8c55cbbb5
norshukri <norshukri87@gmail.com>
2024-04-17 20:56:02 +0800

    Activity 3.4

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/hello', (req, res) => {
+app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/hello', (req, res) => {
+  res.send('Hello World!2')
 })
 
 app.get('/heybaby', (req, res) => {
